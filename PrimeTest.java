1a1ebfd5b2f1fc940705a7f50738285603fdfd69
hoju-os <47367882+hoju-os@users.noreply.github.com>
2019-02-13 19:32:05 -0500

    Update PrimeTest.java

--- PrimeTest.java
+++ PrimeTest.java
@@ -3,7 +3,7 @@ import java.util.stream.IntStream;
 public class PrimeTest {
     public static void main(String[] args) throws InterruptedException {
         //int[] a = IntStream.range(1, 5).toArray();
-        int [] a = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+        int [] a = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
         int numThreads = 2;
 
         int s = (a.length) / numThreads;
@@ -17,7 +17,7 @@ public class PrimeTest {
 
         for (int i = 0; i < numThreads; i++) {
             init = i * s;
-            end = (i < numThreads - 1) ? ((i + 1) * s - 1) : (n);
+            end = (i < numThreads - 1) ? ((i + 1) * s - 1) : (n-1);
             threads[i] = new PrimeThread("T" + i, a, init, end);
             threads[i].start();
             threads[i].join();
